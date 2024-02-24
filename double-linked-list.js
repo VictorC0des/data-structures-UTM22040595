5eec085885222fd85973444477b6daf2abe548af
Victor Carmona <victor181851@gmail.com>
2024-02-24 15:53:46 -0600

    feat: doubleLinkedList class was created and also append method

---
+++ double-linked-list.js
@@ -0,0 +1,46 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+        this.prev =  null;
+    }
+}
+
+class DoubleLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+    }
+
+    append(data){
+        if(!data){
+            return "No data"
+        }
+        const newNode = new Node(data)
+
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else{
+            newNode.prev = this.tail
+            this.tail.next = newNode;
+            this.tail = newNode
+        }
+
+
+    }
+
+    prepend(data){
+        if(!data){
+            return "No data"
+        }
+        const newNode = new Node(data)
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+
+        }
+    }
+
+}
